29119f6852f6a83711dd19f63600f12902a1d0cf
jnssterrass <jnssterrass@gmail.com>
2014-02-13 13:31:42 +0100

    Update Recommendation.java

--- xarxa/src/main/java/lin/Recommendation.java
+++ xarxa/src/main/java/lin/Recommendation.java
@@ -1,26 +1,15 @@
 package lin;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
 
 public class Recommendation {
 
-	private name;
+	private String name;
 	
-	public setName(String n) {
+	public void setName(String n) {
 		name = n;
 	}
 	
-	public getName() {
+	public String getName() {
 		return name;
 	}
 }
