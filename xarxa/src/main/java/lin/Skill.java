cc10e2d2f15de42568f738d759ba20848c69fc3a
jnssterrass <jnssterrass@gmail.com>
2014-02-13 13:18:20 +0100

    Create Skill.java

---
+++ xarxa/src/main/java/lin/Skill.java
@@ -0,0 +1,26 @@
+package lin;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+public class Skill {
+
+	private name;
+	
+	public setName(String n) {
+		name = n;
+	}
+	
+	public getName() {
+		return name;
+	}
+}
